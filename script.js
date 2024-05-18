ea5c8a9e841b6ab90dfce0bd3aff2371d02d6792
Fabio-Oliveira-Dev <fabiooliveiraideia2@gmail.com>
2024-05-18 16:19:41 -0300

    logica de envio mensagem p/ api whatsapp e horario aberto/fechado

--- script.js
+++ script.js
@@ -146,12 +146,48 @@ addressInput.addEventListener("input", function(event){
     }
 })
 
-// adicionar e remover texto de mensagem do input
+// finalizar pedido no carrinho
 checkoutBtn.addEventListener("click", function(){
+    // const isOpen = checkRestaurantOpen();
+    // if(!isOpen){
+    //     alert("RESTAURANTE FECHADO NO MOMENTO!")
+    //     return;
+    // }
+
     if(cart.length === 0) return;
     if(addressInput.value === ""){
         addressWarn.classList.remove("hidden")
         addressInput.classList.add("border-red-500")
         return;
     }
+
+    // enviar pedido para a api do whatsapp
+    const cartItems = cart.map((item)=>{
+        return(
+            `${item.name} Quantidade: (${item.quantity}) Preço: R$ ${item.price} | `
+        )
+    }).join("")
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "21968917243"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
 })
+
+// verificar a hora e manupular o horario
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+}
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500")
+    spanItem.classList.add("bg-green-500")
+} else {
+    spanItem.classList.remove("bg-green-500")
+    spanItem.classList.add("bg-red-500")
+}
